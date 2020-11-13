1e5ca948ea2f30b7388b5635d5f75be45c1d4bf7
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-11-13 09:56:45 +0100

    Adds env vars.

--- frontend/app/config/microservice.php
+++ frontend/app/config/microservice.php
@@ -1,5 +1,27 @@
 <?php
 
-  define('MS_BOOKING', "http://193.180.109.41:30001"); 
-  define('MS_USER', "http://193.180.109.41:30002"); 
-  define('MS_PAYMENT', "http://193.180.109.41:30004"); 
+  //Get env vars
+  $env = array(
+    'MS_BOOKING' => getenv('MS_BOOKING'), 
+    'MS_USER' => getenv('MS_USER'),
+    'MS_PAYMENT' => getenv('MS_PAYMENT')
+  );
+
+  //Define default values
+  $default = array(
+    'MS_BOOKING' => "http://193.180.109.41:30001", 
+    'MS_USER' => "http://193.180.109.41:30002",
+    'MS_PAYMENT' => "http://193.180.109.41:30004"
+  ); 
+
+  //Fallback to default
+  foreach($env as $key => $item) {
+    if($item === false) {
+      $env[$key] = $default[$key]; 
+    }
+  }
+
+  //Set
+  foreach($env as $key => $item) {
+    define($key, $item); 
+  }
